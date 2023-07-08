5761a2170a46615584df33a1dedad1d83cd1aacf
VEDANT JADHAV12 <91117955+VEDANTJADHAV12@users.noreply.github.com>
2023-07-08 18:33:18 +0530

    Add files via upload

---
+++ learn.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+
+public class learn {
+
+     public static void main(String[] args){
+         
+         Scanner sc= new Scanner(System.in);
+         int myNumber =(int)(Math.random()*100);
+         int userNumber=0;
+
+         do{
+             System.out.println("Guess my number :");
+              userNumber = sc.nextInt();
+             if(userNumber==myNumber){
+
+               System.out.println("WoOHO CORRECT!");
+               break;
+             }
+             else if(userNumber > myNumber){
+
+               System.out.println("your number is too large");
+             }
+             else {
+               System.out.println("your numbeer is too small");
+             }
+
+         }while(userNumber >= 0);
+
+         System.out.print("My number was: ");
+         System.out.println(myNumber);
+
+     
+
+
+     
+
+      
+
+
+     }
+
+
+    
+}
